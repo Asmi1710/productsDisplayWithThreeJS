44f3b4e755b57dc3bbefdf72d6ddd4dc070c2111
Asmita Singh <asmita.singh@logipe.com>
2023-07-07 16:12:30 +0530

    basic files

---
+++ client/src/assets/index.js
@@ -0,0 +1,9 @@
+import aiIcon  from "./ai-icon.png";
+import fileIcon from "./file-icon.png";
+import swatchIcon from "./swatch.png";
+import downloadIcon from "./download-icon.png";
+
+import logoShirtIcon from "./logo-tshirt.png";
+import stylishShirtIcon from "./stylish-tshirt.png";
+
+export { aiIcon, fileIcon, swatchIcon, downloadIcon, logoShirtIcon, stylishShirtIcon };
